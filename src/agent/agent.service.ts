48f90fe34bf92f1420f65a5fed6449c6b58008db
Asad-bot07 <asadhussain2408@gmail.com>
2026-01-10 22:46:25 +0530

    handoff added

--- src/agent/agent.service.ts
+++ src/agent/agent.service.ts
@@ -1,38 +1,29 @@
 import "dotenv/config";
 import { Agent, run } from "@openai/agents";
-import { getOwners, getPlans } from "../tool/tools.js";
+// import { getOwners, getPlans } from "../tool/tools.js";
 import { refundAgent } from "./refund.agent.js";
 import { checkTeamVacancy } from "./checkVacancy.agent.js";
 import { bookingAgent } from "./booking.agent.js";
-
+import { salesAgent } from "./sales.agent.js";
 
 const mainAgent = new Agent({
-  name: "Sales Agent",
+  name: "reception agent",
   instructions:
-    "You are an expert sales agent who only deals with sales related queries for an media agency company named Pixelhouse, talk to the user and help them in what they need",
-  tools: [
-    getPlans,
-    refundAgent.asTool({
-      toolName: "refund expert",
-      toolDescription: "handles refund queries and requests",
-    }),
-    checkTeamVacancy.asTool({
-      toolName: "check team availbility",
-      toolDescription:
-        "checks data and tell wheather a photographer or videographer is available or not",
-    }),
-    getOwners,
-    bookingAgent.asTool({
-      toolName: "book shoot for user",
-      toolDescription: "books the shoot for the user",
-    }),
-  ],
+    "You are the receptionist expert in understanding what does the customer needs and then route them or handoff to the right agent directly",
+  handoffDescription: `you have 4 agents available : 
+  - salesAgent : Experts in handling queries related to sales like plans, pricing available. good for first commer
+  - refundAgent : experts in initiating refunds for the customers, taking username and issues for our betterment
+  - checkTeamVacancy : experts in managing the availability of our team members which helps user to schedule their shoot
+  - bookingAgent : experts in booking shoots for the customers and assign them members based on their availability
+  `,
+  handoffs : [salesAgent, refundAgent, checkTeamVacancy, bookingAgent]
 });
 
-async function runAgent(query : string) {
+async function runAgent(query: string) {
   const result = await run(mainAgent, query);
   console.log(result.finalOutput);
+  // console.log(result.history);
 }
 
-const query = "I want to book a reel shoot for tommorow, under 1299";
+const query : string = "Hey there, my name is Asad Hussain with userid #2408 and i want to have a refund request as the shoot has been cancelled due to some reasons";
 runAgent(query);
